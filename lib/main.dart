ba8359ffceaba86dc284a83c05887c09900223d2
mojaveOS <mojaveos@mojaveOSs-MacBook-Pro.local>
2020-09-17 00:23:34 +0100

    added dice roll sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   return runApp(
@@ -6,7 +9,10 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          centerTitle: true,
+          title: Text(
+            'Dicee',
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +21,57 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+//void _switchDice() {}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1, rightDiceNumber = 1;
+  AudioCache player;
+
+  void _trySwitch() {
+    setState(() {
+      player.play('MOREDICE.wav');
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    player = AudioCache(
+        prefix: "audio/",
+        fixedPlayer: AudioPlayer()..setReleaseMode(ReleaseMode.STOP));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                player.play('MOREDICE.wav');
+                _trySwitch();
+              },
+              child: Image.asset('assets/images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                _trySwitch();
+              },
+              child: Image.asset('assets/images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
